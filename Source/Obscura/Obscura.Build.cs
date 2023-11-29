f6480ded9952bd0021d9ad1af9625e0300ae165c
ST <stulunayy@gmail.com>
2023-11-29 15:59:11 +0100

    Obscura Log system

--- Source/Obscura/Obscura.Build.cs
+++ Source/Obscura/Obscura.Build.cs
@@ -8,6 +8,6 @@ public class Obscura : ModuleRules
 	{
 		PCHUsage = PCHUsageMode.UseExplicitOrSharedPCHs;
 
-		PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore", "HeadMountedDisplay", "EnhancedInput" });
+		PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore", "HeadMountedDisplay", "EnhancedInput", "Json", "JsonUtilities"});
 	}
 }
