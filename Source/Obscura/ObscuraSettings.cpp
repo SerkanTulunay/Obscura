ad17c38e22a839b37bb93358a31c492429a5ab0a
MelvinLagerman <104003474+MelvinLagerman@users.noreply.github.com>
2023-11-29 17:17:58 +0100

    SettingsScript

--- Source/Obscura/ObscuraSettings.cpp
+++ Source/Obscura/ObscuraSettings.cpp
@@ -3,3 +3,43 @@
 
 #include "ObscuraSettings.h"
 
+
+UObscuraSettings::UObscuraSettings(const FObjectInitializer& ObjectInitializer): Super(ObjectInitializer)
+{
+	//Charging enemy
+	ChargingEnemyMoveSpeed = 100.f;
+
+	ChargingEnemyVolume =0.5f;
+
+	//Attacking enemy
+	AttackingEnemyMoveSpeed = 100.f;
+
+	AttackingEnemyRange= 300.f;
+
+	AttackingEnemyAttackDelay =4.f;
+
+	AttackingEnemyIdleVolume = 0.6f;
+	
+	//Patrolling enemy
+
+	PatrollingEnemyMoveSpeed =100.f;
+
+	PatrollingEnemyRange = 300.f;
+
+	PatrollingEnemyAttackDelay=4.f;
+	
+	PatrollingEnemyIdleVolume = 0.2f;
+
+	//Player
+	MoveCD =0.3f;
+
+	StunCooldown =2.5f;
+
+}
+
+UObscuraSettings* UObscuraSettings::GetSettings()
+{
+	return Cast<UObscuraSettings>(UObscuraSettings::GetGameUserSettings());
+	
+
+}
